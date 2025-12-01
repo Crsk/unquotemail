46796150f42612733bbf78a933e9d5de63e53d1c
Christopher Kiessling <crsk@mail.com>
2025-11-30 22:33:01 -0300

    chore: remove python files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    include: ['ts-tests/**/*.test.ts'],
+    include: ['tests/**/*.test.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
