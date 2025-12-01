ae23d171338da826e263442d2c21af4ae90cc5b2
Christopher Kiessling <crsk@mail.com>
2025-11-30 22:10:42 -0300

    feat: convert to TS

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Unquote, VerboseUnquote } from './unquote';
+export type { UnquoteOptions } from './unquote';
+export { patterns } from './patterns';
+export { htmlToText } from './htmlToText';
